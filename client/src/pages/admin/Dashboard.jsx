677daa0695e61df93922b53bac4971a05ecb2762
abdulnoor <abdulnoorwork@gmail.com>
2026-04-05 19:11:22 +0500

    saved

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -54,40 +54,40 @@ const Dashboard = () => {
       </div>
       {/* blogs */}
       <div className='max-w-4xl'>
-        <div className='flex items-center gap-3 m-4 mt-7 text-[#3e484e]'>
+        <div className='flex items-center gap-3 mb-4 mt-7 text-[#3e484e]'>
           <img src={dashboard_icon_4} alt="" />
-          <p className='text-sm' style={{fontFamily: 'Poppins'}}>Recent News</p>
+          <p className='text-sm' style={{ fontFamily: 'Poppins' }}>Recent News</p>
         </div>
-        {blogs.length>0 ? 
-        <div className='relative w-full text-sm overflow-x-auto shadow rounded-lg scrollbar-hide bg-white max-sm:max-h-[70vh]'>
-          <div className='w-full text-sm'>
-            <div className='blog_list_title text-xs uppercase p-3 border-b font-semibold sm:grid lg:grid-cols-[1fr_3fr_1fr_1fr_1fr] sm:grid-cols-[1fr_3fr_1fr_1fr] gap-2 text-[#3e484e] font-medium hidden'>
-              <label>News</label>
-              <label>Title</label>
-              <label className='mx-auto'>Category</label>
-              <label className=' max-lg:hidden mx-auto'>Date</label>
-              <label className='mx-auto'>Action</label>
+        <div className='blog_list_title text-xs uppercase p-3 border-b font-semibold sm:grid lg:grid-cols-[1fr_3fr_1fr_1fr_1fr] sm:grid-cols-[1fr_3fr_1fr_1fr] gap-2 text-[#3e484e] font-medium hidden'>
+          <label>News</label>
+          <label>Title</label>
+          <label className='mx-auto'>Category</label>
+          <label className=' max-lg:hidden mx-auto'>Date</label>
+          <label className='mx-auto'>Action</label>
+        </div>
+        {blogs.length > 0 ?
+          <div className='relative w-full text-sm overflow-x-auto shadow rounded-lg scrollbar-hide bg-white max-sm:max-h-[70vh]'>
+            <div className='w-full text-sm'>
+              {blogs.length > 0 ?
+                <div>
+                  {blogs?.slice(length - 6).reverse().map((blog, index) => (
+                    <div key={index} className='blog_list border-b text-[#4e5c64] font-medium border-gray-300 text-sm sm:p-3 p-4 grid lg:grid-cols-[1fr_3fr_1fr_1fr_1fr] sm:grid-cols-[1fr_3fr_1fr_1fr] gap-2.5 items-center'>
+                      <figure className=''>
+                        <img className='main_image sm:h-9 sm:w-[70px] h-[60px] max-sm:mx-auto' src={blog.image} alt="" />
+                      </figure>
+                      <p className='leading-[1.3em] max-sm:w-[200px] max-sm:text-center max-sm:mx-auto' style={{ fontFamily: 'Urbanist' }}>{blog.title}</p>
+                      <p className='leading-[1.3em] mx-auto max-sm:text-[#249991]' style={{ fontFamily: 'Urbanist' }}>{blog.category}</p>
+                      <p className=' max-lg:hidden mx-auto' style={{ fontFamily: 'Urbanist' }}>{new Date(blog.created_At).toDateString()}</p>
+                      <figure className=' flex text-sm items-center gap-2 mx-auto'>
+                        <img src={edit_icon} onClick={() => { navigate(`/admin/updateblog/${blog._id}`) }} alt="" className='md:h-[20px] md:w-[20px] h-5 W-5 hover:scale-110 transition-all cursor-pointer' />
+                        <img src={cross_icon} onClick={() => deleteBlog(blog._id)} alt="" className='md:h-[20px] md:w-[20px] h-5 W-5 border border-red-400 rounded-full hover:scale-110 transition-all cursor-pointer' />
+                      </figure>
+                    </div>
+                  ))}
+                </div> :
+                <div className='text-gray-800 font-medium text-[16px] text-center py-10'>You don,t have any blogs</div>}
             </div>
-            {blogs.length>0 ? 
-            <div>
-              {blogs?.slice(length - 6).reverse().map((blog, index) => (
-                <div key={index} className='blog_list border-b text-[#4e5c64] font-medium border-gray-300 text-sm sm:p-3 p-4 grid lg:grid-cols-[1fr_3fr_1fr_1fr_1fr] sm:grid-cols-[1fr_3fr_1fr_1fr] gap-2.5 items-center'>
-                  <figure className=''>
-                    <img className='main_image sm:h-9 sm:w-[70px] h-[60px] max-sm:mx-auto' src={blog.image} alt="" />
-                  </figure>
-                  <p className='leading-[1.3em] max-sm:w-[200px] max-sm:text-center max-sm:mx-auto' style={{fontFamily:'Urbanist'}}>{blog.title}</p>
-                  <p className='leading-[1.3em] mx-auto max-sm:text-[#249991]' style={{fontFamily:'Urbanist'}}>{blog.category}</p>
-                  <p className=' max-lg:hidden mx-auto' style={{fontFamily:'Urbanist'}}>{new Date(blog.created_At).toDateString()}</p>
-                  <figure className=' flex text-sm items-center gap-2 mx-auto'>
-                    <img src={edit_icon} onClick={() => { navigate(`/admin/updateblog/${blog._id}`) }} alt="" className='md:h-[20px] md:w-[20px] h-5 W-5 hover:scale-110 transition-all cursor-pointer' />
-                    <img src={cross_icon} onClick={() => deleteBlog(blog._id)} alt="" className='md:h-[20px] md:w-[20px] h-5 W-5 border border-red-400 rounded-full hover:scale-110 transition-all cursor-pointer' />
-                  </figure>
-                </div>
-              ))}
-            </div> : 
-            <div className='text-gray-800 font-medium text-[16px] text-center py-10'>You don,t have any blogs</div>}
-          </div>
-        </div> : <div className='font-medium min-h-[100px] text-sm flex items-center justify-center text-center bg-white rounded-md max-w-2xl'>You don,t have any blogs</div>}
+          </div> : <div className='font-medium min-h-[100px] text-sm flex items-center justify-center text-center bg-white rounded-md max-w-2xl'>You don,t have any blogs</div>}
       </div>
     </div >
   )
