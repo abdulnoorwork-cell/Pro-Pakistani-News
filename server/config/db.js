a807fac98ec518062c686ea8d5b3626dc5ac9d34
abdulnoor <abdulnoorwork@gmail.com>
2026-04-19 13:41:28 +0500

    saved

--- server/config/db.js
+++ server/config/db.js
@@ -11,7 +11,7 @@ if (!global.dbPool) {
         database: process.env.DB_DATABAE,
         port: process.env.DB_PORT,
         waitForConnections: true,
-        connectionLimit: 2,
+        connectionLimit: 1,
         queueLimit: 0,
         enableKeepAlive: true,
         keepAliveInitialDelay: 0
